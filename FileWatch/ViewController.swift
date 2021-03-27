329098b5830a9b6542985c95eb9a46c082b23291
kloostermanw <wiebe@kloosterman.eu>
2021-03-27 13:29:36 -0400

    Initial Commit

---
+++ FileWatch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  FileWatch
+//
+//  Created by Wiebe Kloosterman on 27/03/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
