9da098124bd531cd08004117a2c5c72af9a696fc
kloostermanw <wiebe@kloosterman.eu>
2021-03-28 13:46:15 -0400

    first commit

---
+++ FileWatch/CustomTableCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableCell.swift
+//  FileWatch
+//
+//  Created by Wiebe Kloosterman on 27/03/2021.
+//
+
+import Cocoa
+
+class CustomTableCell: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
